8f973894f42f63de000563638815bf3935479e84
Darius Barbano <darius.barbano@gmail.com>
2022-07-22 18:04:21 -0400

    more

--- src/init.el
+++ src/init.el
@@ -64,13 +64,12 @@
 (load-tilemacs-modules dependency-modules)
 
 ;; Initialize lsp and various language-specific modes
-(defun hook-to-lsp (hook)
+(defun hook-lsp (hook)
  (add-hook hook #'lsp))
 
 (defun hook-language-mode-lsp (filename-regex mode-hook mode-callback)
-    (add-to-list 'auto-mode-alist '(filename-regex . mode-callback))
-    (hook-to-lsp mode-hook)
-)
+    (add-to-list 'auto-mode-alist '(,filename-regex . mode-callback))
+    (hook-lsp mode-hook))
 
 (hook-language-mode-lsp "\\.rs\\'" 'rust-mode-hook 'rust-mode)
 (hook-language-mode-lsp "\\.tsx?\\'" 'typescript-mode-hook 'typescript-mode)
@@ -79,7 +78,7 @@
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
-(load-theme 'doom-one t)
+(load-theme 'doom-dark+ t)
 
 ;; Enable flashing mode-line on errors
 (doom-themes-visual-bell-config)
@@ -91,6 +90,7 @@
 ;; Corrects (and improves) org-mode's native fontification.
 (doom-themes-org-config)
 
+(toggle-debug-on-error)
 
 ;; Configure windows, tabs, and posframes
 
@@ -210,3 +210,15 @@
 ;;   )
 
 ;; (prin1 "Emacs initialization complete.")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((lsp-mode) (lsp-mode) (lsp-mode) (lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
