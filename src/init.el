9686084baa906978b1597f95fa9a744ba5481e50
Darius Barbano <darius.barbano@gmail.com>
2022-07-22 23:40:12 -0400

    more refactor

--- src/init.el
+++ src/init.el
@@ -43,6 +43,7 @@
 
    ;;; External Modules
    '((symbol . doom-themes))
+   '((symbol . all-the-icons))
    '((symbol . f))
    '((symbol . lsp-mode))
    '((symbol . lsp-ui))
@@ -68,13 +69,36 @@
  (add-hook hook #'lsp))
 
 (defun hook-language-mode-lsp (filename-regex mode-hook mode-callback)
-    (add-to-list 'auto-mode-alist '(,filename-regex . mode-callback))
+    (add-to-list 'auto-mode-alist '(filename-regex . mode-callback))
     (hook-lsp mode-hook))
 
 (hook-language-mode-lsp "\\.rs\\'" 'rust-mode-hook 'rust-mode)
 (hook-language-mode-lsp "\\.tsx?\\'" 'typescript-mode-hook 'typescript-mode)
 
 ;; Configure aesthetics
+
+;; Fix system clipboard interoperability
+(setq select-enable-clipboard t)
+(setq interprogram-paste-function 'x-selection-value)
+
+;; Remove GUI clutter
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Line numbers
+(global-display-line-numbers-mode t)
+(column-number-mode t)
+
+;; Windows
+(window-divider-mode)
+
+;; Text Appearance
+(setq tab-width 4)
+(setq frame-resize-pixelwise t)
+(set-face-attribute 'default nil :height 120)
+
+
 ;; Global settings (defaults)
 (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
       doom-themes-enable-italic t) ; if nil, italics is universally disabled
@@ -85,12 +109,10 @@
 ;; Enable custom neotree theme (all-the-icons must be installed!)
 (doom-themes-neotree-config)
 ;; or for treemacs users
-(setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+(setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
 (doom-themes-treemacs-config)
 ;; Corrects (and improves) org-mode's native fontification.
 (doom-themes-org-config)
-
-(toggle-debug-on-error)
 
 ;; Configure windows, tabs, and posframes
 
@@ -113,7 +135,39 @@
 
 
 ;; Evil mode bindings
+(evil-mode 1)
+(setq evil-default-state 'normal)
 
+(defun kill-all-buffers ()
+  "Kills all buffers."
+  (interactive)
+  (mapc (lambda (buffer)
+	  (progn
+	    (set-buffer buffer)
+	    (kill-buffer buffer))) (buffer-list)))
+
+
+;; Set the evil leader key to <SPC>
+(evil-set-leader 'normal (kbd "<SPC>") 'nil)
+
+(evil-global-set-key 'normal (kbd "<leader> <left>") 'awesome-tab-backward)
+(evil-global-set-key 'normal (kbd "<leader> <right>") 'awesome-tab-forward)
+
+(evil-global-set-key 'normal (kbd "<leader> q <left>") 'awesome-tab-move-current-tab-to-left)
+(evil-global-set-key 'normal (kbd "<leader> q <right>") 'awesome-tab-move-current-tab-to-right)
+
+(evil-global-set-key 'normal (kbd "<leader> b k") 'kill-buffer)
+(evil-global-set-key 'normal (kbd "<leader> b s") 'save-buffer)
+(evil-global-set-key 'normal (kbd "<leader> b f") 'find-file)
+
+(evil-global-set-key 'normal (kbd "<leader> t o") 'treemacs)
+(evil-global-set-key 'normal (kbd "<leader> t d") 'treemacs-select-directory)
+
+(evil-global-set-key 'normal (kbd "<leader> w <left>") 'windmove-left)
+(evil-global-set-key 'normal (kbd "<leader> w <right>") 'windmove-right)
+(evil-global-set-key 'normal (kbd "<leader> w <up>") 'windmove-up)
+(evil-global-set-key 'normal (kbd "<leader> w <down>") 'windmove-down)
+(evil-global-set-key 'normal (kbd "<leader> w k") 'delete-window)
 
 
 ;; ;; --- Initialize Tilemacs ---
